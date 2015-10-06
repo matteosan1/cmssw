07d70eedc9361ad359f24f680383a19d832d6162
Matteo <matteosan1@gmail.com>
2015-10-06 09:20:04 +0200

    fix cand cut in Nm1Selector

--- PhysicsTools/TagAndProbe/plugins/Nm1Selector.cc
+++ PhysicsTools/TagAndProbe/plugins/Nm1Selector.cc
@@ -82,25 +82,27 @@ void Nm1Selector<T>::produce(edm::Event & event, const edm::EventSetup & setup) 
   for (unsigned int i = 0; i < h_inputs->size(); ++i) {
 
     candidateRef ptr = (*h_inputs)[i];
-    vid::CutFlowResult fullCutFlowData = (*cutflow)[ptr];
 
-    bool pass = false;
-    if (cutIndicesToMask_.size() > 0) {
-      vid::CutFlowResult maskedCutFlowData = fullCutFlowData.getCutFlowResultMasking(cutIndicesToMask_);
-      if (maskedCutFlowData.cutFlowPassed())
-	pass = true;
-      //std::cout << pass << std::endl;
-      //int ncuts = maskedCutFlowData.cutFlowSize();
-      //for(int icut = 0; icut<ncuts; icut++) {
-      //	std::cout << icut << " " << maskedCutFlowData.getNameAtIndex(icut).c_str() 
-      //		  << (int)maskedCutFlowData.isCutMasked(icut) << " " 
-      //		  << maskedCutFlowData.getValueCutUpon(icut) << " " 
-      //		  << (int)maskedCutFlowData.getCutResultByIndex(icut) << std::endl;
-      //}
-    } else {
-      vid::CutFlowResult maskedCutFlowData = fullCutFlowData.getCutFlowResultMasking(cutNamesToMask_);
-      if (maskedCutFlowData.cutFlowPassed())
-	pass = true;
+    bool pass = false;    
+    if (candSelector(*ptr)) {
+      vid::CutFlowResult fullCutFlowData = (*cutflow)[ptr];
+      if (cutIndicesToMask_.size() > 0) {
+	vid::CutFlowResult maskedCutFlowData = fullCutFlowData.getCutFlowResultMasking(cutIndicesToMask_);
+	if (maskedCutFlowData.cutFlowPassed())
+	  pass = true;
+	//std::cout << pass << std::endl;
+	//int ncuts = maskedCutFlowData.cutFlowSize();
+	//for(int icut = 0; icut<ncuts; icut++) {
+	//	std::cout << icut << " " << maskedCutFlowData.getNameAtIndex(icut).c_str() 
+	//		  << (int)maskedCutFlowData.isCutMasked(icut) << " " 
+	//		  << maskedCutFlowData.getValueCutUpon(icut) << " " 
+	//		  << (int)maskedCutFlowData.getCutResultByIndex(icut) << std::endl;
+	//}
+      } else {
+	vid::CutFlowResult maskedCutFlowData = fullCutFlowData.getCutFlowResultMasking(cutNamesToMask_);
+	if (maskedCutFlowData.cutFlowPassed())
+	  pass = true;
+      }
     }
 
     if (pass)
